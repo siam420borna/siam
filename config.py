fa8abf1c9cf39edfc8f471cd693bfda2da27963c
siam420borna <siam420borna@gmail.com>
2025-06-01 18:16:11 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("20103040"))
-    API_HASH = os.getenv("8c4162dedd5303a8e3fe36257e40d1f6")
-    BOT_TOKEN = os.getenv("7909791290:AAHbaeT9PjUeGdUNPs3FJRZjaUStat28V40")
-    MONGODB_URL = os.getenv("mongodb+srv://siam420:siam420@cluster0.ov6coln.mongodb.net/?retryWrites=true&w=majority")
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    MONGODB_URL = os.getenv("MONGODB_URL")
