76e441b8ed46e4627843ba3e7ef1a6ae2e8bad1f
siam420borna <siam420borna@gmail.com>
2025-06-01 17:35:22 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", 12345))
-    API_HASH = os.getenv("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-    MONGODB_URL = os.getenv("MONGODB_URL", "your_mongodb_uri")
+    API_ID = int(os.getenv("API_ID", 20103040))
+    API_HASH = os.getenv("API_HASH", "8c4162dedd5303a8e3fe36257e40d1f6")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7728627887:AAFuVSYKBwVnW0jjcSaXs35k8txIP2HtF6A")
+    MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://siam420:siam420@cluster0.zcqe3zx.mongodb.net/?retryWrites=true&w=majority")
 
     # Default user settings
     DEFAULT_SETTINGS = {
